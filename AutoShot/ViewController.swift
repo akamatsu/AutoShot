d5d83efd4abc42aa517e81153c01488b5e55d5cf
Masayuki Akamatsu <aka@iamas.ac.jp>
2023-02-08 16:47:30 +0900

    Implements basic functions

--- AutoShot/ViewController.swift
+++ AutoShot/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    let picker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+            picker.showsCameraControls = false
+            picker.allowsEditing = true
+            picker.delegate = self
+            self.present(picker, animated: false)
+            Timer.scheduledTimer(withTimeInterval: 3.0, repeats: false, block: { (timer:Timer) in
+                self.picker.takePicture()
+            })
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            UIImageWriteToSavedPhotosAlbum(image, self, nil, nil)
+        }
+        self.dismiss(animated: false)
+        UIApplication.shared.perform(#selector(NSXPCConnection.suspend))
+    }
 }
-
