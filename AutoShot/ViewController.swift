ec85c0c69ab4f4c000a413847748d0ccc6d55749
Masayuki Akamatsu <aka@iamas.ac.jp>
2023-02-09 09:50:30 +0900

    Use AVCaptureSessionDidStartRunning

--- AutoShot/ViewController.swift
+++ AutoShot/ViewController.swift
@@ -13,12 +13,25 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     let picker = UIImagePickerController()
     
     override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        openCamera()
+    }
+    
+    func openCamera () {
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
             picker.sourceType = .camera
             picker.showsCameraControls = false
             picker.allowsEditing = true
             picker.delegate = self
             self.present(picker, animated: false)
+            
+            NotificationCenter.default.addObserver(self, selector: #selector(self.takePicture), name: NSNotification.Name.AVCaptureSessionDidStartRunning, object: nil)
+        }
+    }
+    
+    @objc func takePicture() {
+        NotificationCenter.default.removeObserver(self, name:NSNotification.Name.AVCaptureSessionDidStartRunning, object: nil)
+        DispatchQueue.main.async {
             Timer.scheduledTimer(withTimeInterval: 3.0, repeats: false, block: { (timer:Timer) in
                 self.picker.takePicture()
             })
